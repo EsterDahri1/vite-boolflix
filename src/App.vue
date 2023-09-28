e187ede106015e05be44a70f434fdb0b77b17b05
EsterDahri <esterdahri@gmail.com>
2023-09-28 12:51:13 +0200

    add input and search button

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 
 </script>
 
-<template></template>
+<template>
+    <div class="container p-2">
+        <div class="d-flex align-items-center justify-content-center">
+            <input type="text" class="form-control">
+            <button class="ms-3 btn btn-primary">Search</button>
+        </div>
+    </div>
+</template>
 
 <style scoped></style>
