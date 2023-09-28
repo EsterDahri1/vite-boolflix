7c87c499c1b6504c820ef3d7ff7cc6abc8d435c9
EsterDahri <esterdahri@gmail.com>
2023-09-28 12:43:15 +0200

    add general state document

--- src/state.js
+++ src/state.js
@@ -1,5 +1,5 @@
 import { reactive } from vue;
 import axios from 'axios';
 export const state = reactive({
-    url: '',
+    url: 'https://api.themoviedb.org/3/search/movie?api_key=e10a79c4628cb2c1d4e74a07721be7c3&query=',
 })
