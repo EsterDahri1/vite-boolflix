3f01ddf1e7ad7a4f84e4a25daa33ecdb132c46df
EsterDahri <esterdahri@gmail.com>
2023-09-29 10:55:54 +0200

    delete everything and try to do it again

--- src/App.vue
+++ src/App.vue
@@ -8,31 +8,10 @@ export default {
         }
     },
 
-    methods: {
-        searchMovieByName() {
-            const urlByName = this.state.url + this.state.movieTitle;
-            this.state.fetchMovieByName(urlByName)
-        }
-    }
 
 }
 </script>
 
-<template>
-    <div class="container p-2">
-        <div class="d-flex align-items-center justify-content-center">
-            <input type="search" class="form-control" id="name" v-model="this.state.movieTitle">
-            <button @click="searchMovieByName()" class="ms-3 btn btn-primary">Search</button>
-        </div>
-        <ul>
-            <li v-for="result in this.state.results">
-                <span>Titolo: {{ result.title }}</span>
-                <span>Titolo Originale: {{ result.original_title }}</span>
-                <span>Lingua: {{ result.original_language }}</span>
-                <span>Voto: {{ result.vote_average }}</span>
-            </li>
-        </ul>
-    </div>
-</template>
+<template></template>
 
 <style scoped></style>
--- src/state.js
+++ src/state.js
@@ -1,18 +1,5 @@
 import { reactive } from "vue";
 import axios from 'axios';
 export const state = reactive({
-    url: 'https://api.themoviedb.org/3/search/movie?api_key=e10a79c4628cb2c1d4e74a07721be7c3&query=',
-    movieTitle: '',
-    results: [],
-    fetchMovieByName(url) {
-        axios
-            .get(url)
-            .then(response => {
-                this.results = response.data.results
-            })
-            .catch(error => {
-                console.error(error);
-                this.error = error.message
-            })
-    }
+
 })
