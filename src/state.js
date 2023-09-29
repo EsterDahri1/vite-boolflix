df833c5cd411d06ef98e37471debdebea5030441
EsterDahri <esterdahri@gmail.com>
2023-09-29 11:12:29 +0200

    add fetchmovie in state.js

--- src/state.js
+++ src/state.js
@@ -1,5 +1,17 @@
 import { reactive } from "vue";
 import axios from 'axios';
 export const state = reactive({
-
+    base_url: 'https://api.themoviedb.org/3/search/movie?api_key=e10a79c4628cb2c1d4e74a07721be7c3&query=',
+    query: '',
+    movies: '',
+    fetchMovie(url) {
+        axios
+            .get(url)
+            .then(response => {
+                this.movies = response.data.results
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
 })
