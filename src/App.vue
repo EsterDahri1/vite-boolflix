8ce2b2804e1610d8bfb199d89ace4e35ffb8d64c
EsterDahri <esterdahri@gmail.com>
2023-10-02 11:04:01 +0200

    add header and info in card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script >
 import { state } from "./state.js";
+import AppHeader from "./components/AppHeader.vue"
 export default {
     name: 'App',
     data() {
@@ -9,98 +10,95 @@ export default {
             imgUrl: 'https://image.tmdb.org/t/p/w342',
         }
     },
-    methods: {
-        searchMovie() {
-            const urlMovie = this.state.movie_url + this.state.query;
-            this.state.fetchMovie(urlMovie)
-        },
-        searchTvSeries() {
-            const urlTvSeries = this.state.tv_url + this.state.query;
-            this.state.fetchTvSeries(urlTvSeries)
-        }
-    },
+    components: {
+        AppHeader
+    }
 
 }
 </script>
 
 <template>
-    <div class="container">
-        <div class="">
-            <input type="search" name="search" id="searchMovie" v-model="this.state.query">
-            <button class="btn btn-primary" @click="this.searchMovie(), this.searchTvSeries()">Search</button>
-        </div>
-        <h2>FILM</h2>
-        <div class="row row-cols-4">
+    <body>
+        <AppHeader />
+        <div class="container">
 
-            <div class="card" v-for="movie in state.movies">
-                <img :src="imgUrl + movie.poster_path" alt="" class="img-card-top">
-                <div class="card-body">
-                    <span> Titolo: {{ movie.title }} </span>
-                    <span> Titolo originale: {{ movie.original_title }}</span>
-                    <div v-if="this.flags.includes(movie.original_language)">
-                        Lingua: <img width="50" :src="`src/assets/img/flags/${movie.original_language}.png`">
-                    </div>
-                    <span v-else> Lingua: {{ movie.original_language }}</span>
+            <h2>FILM</h2>
+            <div class="row row-cols-4">
 
-                    <span>
-                        Voto:
-                        <div>
-                            <svg v-for="star in Number((movie.vote_average / 2).toFixed(0))"
-                                xmlns="http://www.w3.org/2000/svg" height="1em"
-                                viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                                <path
-                                    d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-                            </svg>
-
-                            <svg v-for="emptyStar in (5 - (Number((movie.vote_average / 2).toFixed(0))))"
-                                xmlns="http://www.w3.org/2000/svg" height="1em"
-                                viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                                <path
-                                    d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.6 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z" />
-                            </svg>
+                <div class="card" v-for="movie in state.movies">
+                    <img :src="imgUrl + movie.poster_path" alt="" class="img-card-top">
+                    <div class="card-body">
+                        <span> Titolo: {{ movie.title }} </span>
+                        <span> Titolo originale: {{ movie.original_title }}</span>
+                        <div v-if="this.flags.includes(movie.original_language)">
+                            Lingua: <img width="50" :src="`src/assets/img/flags/${movie.original_language}.png`">
                         </div>
-                    </span>
+                        <span v-else> Lingua: {{ movie.original_language }}</span>
+
+                        <span>
+                            Voto:
+                            <div>
+                                <svg v-for="star in Number((movie.vote_average / 2).toFixed(0))"
+                                    xmlns="http://www.w3.org/2000/svg" height="1em"
+                                    viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path
+                                        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                                </svg>
+
+                                <svg v-for="emptyStar in (5 - (Number((movie.vote_average / 2).toFixed(0))))"
+                                    xmlns="http://www.w3.org/2000/svg" height="1em"
+                                    viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path
+                                        d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.6 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z" />
+                                </svg>
+                            </div>
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
 
 
-        <h2>SERIE TV</h2>
-        <div class="row row-cols-4">
-            <div class="card" v-for="tvSerie in state.tvSeries">
-                <img :src="imgUrl + tvSerie.poster_path" alt="" class="img-card-top">
-                <div class="card-body">
-                    <span> Titolo: {{ tvSerie.name }} </span>
-                    <span> Titolo originale: {{ tvSerie.original_name }}</span>
-                    <div v-if="this.flags.includes(tvSerie.original_language)">
-                        Lingua: <img width="50" :src="`src/assets/img/flags/${tvSerie.original_language}.png`">
-                    </div>
-                    <span v-else> Lingua: {{ tvSerie.original_language }}</span>
-
-                    <span>
-                        Voto:
-                        <div>
-                            <svg v-for="star in Number((tvSerie.vote_average / 2).toFixed(0))"
-                                xmlns="http://www.w3.org/2000/svg" height="1em"
-                                viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                                <path
-                                    d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
-                            </svg>
-
-                            <svg v-for="emptyStar in (5 - (Number((tvSerie.vote_average / 2).toFixed(0))))"
-                                xmlns="http://www.w3.org/2000/svg" height="1em"
-                                viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                                <path
-                                    d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.6 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z" />
-                            </svg>
+            <h2>SERIE TV</h2>
+            <div class="row row-cols-4">
+                <div class="card" v-for="tvSerie in state.tvSeries">
+                    <img :src="imgUrl + tvSerie.poster_path" alt="" class="img-card-top">
+                    <div class="card-body">
+                        <span> Titolo: {{ tvSerie.name }} </span>
+                        <span> Titolo originale: {{ tvSerie.original_name }}</span>
+                        <div v-if="this.flags.includes(tvSerie.original_language)">
+                            Lingua: <img width="50" :src="`src/assets/img/flags/${tvSerie.original_language}.png`">
                         </div>
-                    </span>
+                        <span v-else> Lingua: {{ tvSerie.original_language }}</span>
+
+                        <span>
+                            Voto:
+                            <div>
+                                <svg v-for="star in Number((tvSerie.vote_average / 2).toFixed(0))"
+                                    xmlns="http://www.w3.org/2000/svg" height="1em"
+                                    viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path
+                                        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
+                                </svg>
+
+                                <svg v-for="emptyStar in (5 - (Number((tvSerie.vote_average / 2).toFixed(0))))"
+                                    xmlns="http://www.w3.org/2000/svg" height="1em"
+                                    viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                    <path
+                                        d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.6 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z" />
+                                </svg>
+                            </div>
+                        </span>
+                    </div>
                 </div>
             </div>
+
+
         </div>
-
-
-    </div>
+    </body>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+body {
+    font-family: 'Netflix Sans';
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,118 @@
+<script>
+import { state } from '../state.js'
+export default {
+    name: 'Appheader',
+    data() {
+        return {
+            state
+        }
+    },
+    methods: {
+        searchMovie() {
+            const urlMovie = this.state.movie_url + this.state.query;
+            this.state.fetchMovie(urlMovie)
+        },
+        searchTvSeries() {
+            const urlTvSeries = this.state.tv_url + this.state.query;
+            this.state.fetchTvSeries(urlTvSeries)
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <div class="wrapper d-flex justify-content-between align-items-center">
+            <div class="menu-navigation d-flex align-items-center ">
+                <a href="#">
+                    <img width="150" src="../assets/img/logo.png" alt="red netflix logo">
+                </a>
+                <ul class="mt-3 list-unstyled d-flex align-items-center">
+                    <li class="ps-3"><a class="text-decoration-none fs-5" href="#">Home</a></li>
+                    <li class="ps-3"><a class="text-decoration-none fs-5" href="#">Serie Tv</a></li>
+                    <li class="ps-3"><a class="text-decoration-none fs-5" href="#">Film</a></li>
+                    <li class="ps-3"><a class="text-decoration-none fs-5" href="#">Nuovi e popolari</a></li>
+                    <li class="ps-3"><a class="text-decoration-none fs-5" href="#">La mia lista</a></li>
+                    <li class="ps-3"><a class="text-decoration-none fs-5" href="#">Sfoglia per lingua</a></li>
+                </ul>
+            </div>
+            <div class="otherOptions d-flex align-items-center">
+                <div class="ps-2 searchbar d-flex align-items-center">
+                    <button class="btn rounded-0" @click="this.searchMovie(), this.searchTvSeries()">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                            viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                            <path
+                                d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                        </svg>
+                    </button>
+                    <input type="search" name="search" id="searchMovie" v-model="this.state.query"
+                        placeholder="Titolo, persone, generi">
+                </div>
+                <a href="#" class="ps-3 text-decoration-none fs-5">Bambini</a>
+                <a href="#" class="ps-3 text-decoration-none fs-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                        viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path
+                            d="M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z" />
+                    </svg>
+                </a>
+                <div class="ps-3 btn-group">
+                    <button type="button" id="ddBtn" class="btn btn-dark dropdown-toggle fs-4" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        <img width="50px" class="rounded-2" src="../assets/img/Netflix-avatar.png" alt="">
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-left">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+header {
+    background-color: $bx_dark;
+
+    .wrapper {
+        max-width: 1440px;
+        width: 80%;
+        margin: auto;
+    }
+
+    a {
+        color: $bx_light;
+    }
+
+    svg {
+        fill: $bx_light;
+        font-size: 25px;
+    }
+
+    .searchbar {
+        background-color: $bx_dark;
+        color: $bx_lightgrey;
+        border: 1px solid $bx_light;
+
+        #searchMovie {
+            background-color: $bx_dark;
+            color: $bx_lightgrey;
+            border: none;
+            box-shadow: none;
+        }
+    }
+
+    .btn-group {
+        #ddBtn {
+            background-color: $bx_dark;
+            border: none;
+        }
+    }
+}
+</style>
